4f524c418ae318e5e5b0ba2d834f35324be83844
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2020-12-10 17:43:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './component/Login';
+import Login from './src/Login';
 
 export default function App() {
   return (
